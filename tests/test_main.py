2cf163eb36d3c0c410811e8955e36e2e3a94cd54
TrofimovkimIDB2204 <akimakim26459@gmail.com>
2026-03-18 17:57:55 +0300

    tesk

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
 import pytest
-from httpx import AsyncClient
-from main import app  # Импорт твоего FastAPI приложения
+from httpx import AsyncClient, ASGITransport # Добавили ASGITransport
+from main import app 
 
 @pytest.mark.asyncio
 async def test_read_main():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    # Теперь передаем приложение через transport
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
